ad002bdf5aeae7a19f20361d043d35181c1091d6
Alejandra Serrano <jandrax2005@gmail.com>
2024-10-16 20:03:40 +0200

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,17 @@
+package ejercicio2;
+
+public static boolean esPrimo(int num){
+        if(num<=1){
+            return false;
+        }
+        for(int i=2; i<=Math.sqrt(num); i++){
+            if(num % i==0){
+                return false;
+            }
+
+        }
+        return true;
+
+
+    }
+}
